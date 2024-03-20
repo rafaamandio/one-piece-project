8f5934cc17901fdd37271fc15d81c2424b5783e9
Rafael Amandio <rafael.amandio@gmail.com>
2024-03-20 13:41:44 -0300

    Finalização do Projeto, ajustes na tela principal e logo

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,52 @@
 const botoes = document.querySelectorAll(".botao");
+const botaoLogo = document.querySelectorAll(".logo");
 const personagens = document.querySelectorAll(".personagem");
 
-botoes.forEach((botao, indice) => {
-	botao.addEventListener("click", () => {
-		desselecionarBotao();
-		desselecionarPersonagem();
 
-		botao.classList.add("selecionado");
-		personagens[indice].classList.add("selecionado");
-	});
-});
 
 function desselecionarPersonagem() {
-	const personagemSelecionado = document.querySelector(".personagem.selecionado");
-	personagemSelecionado.classList.remove("selecionado");
+  const personagemSelecionado = document.querySelector(".personagem.selecionado");
+  if (personagemSelecionado) {
+    personagemSelecionado.classList.remove("selecionado");
+  }
 }
 
 function desselecionarBotao() {
-	const botaoSelecionado = document.querySelector(".botao.selecionado");
-	botaoSelecionado.classList.remove("selecionado");
+  const botaoSelecionado = document.querySelector(".botao.selecionado");
+  if (botaoSelecionado) {
+    botaoSelecionado.classList.remove("selecionado");
+  }
 }
+
+function desselecionarLogo() {
+  const logoSelecionado = document.querySelector(".logo.selecionado");
+  if (logoSelecionado) {
+    logoSelecionado.classList.remove("selecionado");
+  }
+}
+
+botoes.forEach((botao, indice) => {
+  botao.addEventListener("click", () => {
+    desselecionarBotao();
+    desselecionarPersonagem();
+
+
+    botao.classList.add("selecionado");
+    personagens[indice].classList.add("selecionado");
+  });
+});
+
+
+botaoLogo.forEach((botao, indice) => {
+  botao.addEventListener("click", () => {
+    desselecionarBotao();
+    desselecionarPersonagem();
+    desselecionarLogo();
+
+    const imagemPrincipal = document.querySelector(".personagem.principal");
+    imagemPrincipal.classList.add("selecionado");
+    botaoLogo[indice].classList.add("selecionado");
+  });
+});
+
+
